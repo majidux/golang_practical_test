eef4fa5dce787f8b9f7d2aa13d7dbbbd4d6f1be2
Majid Darvishnejad <m.darvishnejad@Tseco.net>
2020-08-16 12:33:53 +0430

    Finish maps

--- src/maps.go
+++ src/maps.go
@@ -1,9 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type doc struct {
+	id   int
+	name string
+	list []string
+}
 
 // maps
 func Maps() {
-	a := make(map[string]int, 10)
-	fmt.Printf("%+v\n", a)
+	a := make(map[string]int)
+	user := doc{
+		id:   1,
+		name: "majid",
+		list: []string{
+			"js",
+			"go",
+		},
+	}
+	for i := 1; i < 10; i++ {
+		if i != 7 {
+			a[fmt.Sprintf("id%v", i)] = i
+		}
+	}
+	delete(a, "id2")
+	fmt.Printf("A: %v\n", a)
+	fmt.Printf("User: %v\n", user)
 }
