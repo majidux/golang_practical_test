cbffeb4d9d952b53e527c903ce4d088c887889c8
majidux <majiddarvishnejad93@gmail.com>
2021-05-10 12:35:01 +0430

    feat: add simple server

---
+++ src/SimpleServer.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("hello go"))
+	})
+	err := http.ListenAndServe(":8080",nil)
+	if err != nil {
+		panic(err.Error())
+	}
+}
